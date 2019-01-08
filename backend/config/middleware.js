f2923ac90cfb509656c2393413e69dcd178c6240
sarahtennis <sarahtennis@Sarahs-MacBook-Air.local>
2019-01-08 16:40:09 -0500

    comments

--- backend/config/middleware.js
+++ backend/config/middleware.js
@@ -5,9 +5,11 @@ const helmet = require("helmet");
 const testRouter = require("../routers/testRouter.js");
 
 module.exports = server => {
+  // middleware
   server.use(express.json());
   server.use(cors());
   server.use(helmet());
 
+  // express routers
   server.use("/test", testRouter);
 };
--- backend/index.js
+++ backend/index.js
@@ -2,5 +2,6 @@ require("dotenv").config(); // load the .env file content
 
 const server = require("./api/server.js");
 
+// uses 7777 unless PORT is set up in environment
 const port = process.env.PORT || 7777;
 server.listen(port, () => console.log(`\nServer listening on port ${port}\n`));
