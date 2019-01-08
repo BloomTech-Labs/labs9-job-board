aa2f10b77c48d7bab9c1a3c6ea620ee58df32d71
Brian G <brian.gregorius@gmail.com>
2019-01-08 17:21:54 -0500

    added components folder and a jobs container file. Set-up router in app.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import JobsContainer from './components/jobsContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      Hello
+        <JobsContainer />
       </div>
     );
   }
---
+++ front-end/src/components/jobsContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import axios from 'axois';
+import { Route } from 'react-router-dom';
+
+
+
+class JobsContainer extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+    };
+
+    render() {
+        return (
+            <div>
+                Hello
+            </div>
+        )
+    }
+}
+
+export default JobsContainer;
