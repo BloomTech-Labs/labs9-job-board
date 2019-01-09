dca80782f4083e9654f36eaf7ee89a926a2340ba
sarahtennis <sarahtennis@Sarahs-MacBook-Air.local>
2019-01-09 12:44:31 -0500

    adds .env to .gitignore, creates Firebase context to prevent multiple instances, provides app with access to Firebase

--- front-end/src/components/Firebase/firebase.js
+++ front-end/src/components/Firebase/firebase.js
@@ -1,5 +1,6 @@
 import app from "firebase/app";
 
+// configuration provided from Firebase website
 const config = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
