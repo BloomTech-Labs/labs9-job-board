e2490c611c84aac60c79368e652ff9be43701e4b
SavannahGreuel <smallidgesavannah@gmail.com>
2019-01-09 16:12:26 -0700

    added sqlite3 locally, made DB connection dynamic, and ready to run migrations to heroku

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,49 +1,38 @@
 // Update with your config settings.
+require('dotenv').config();
+
+
+
+const dbConnection = process.env.DATABASE_URL;
 
 module.exports = {
 
   development: {
-    client: 'pg',
-    connection: 'postgres://sokhsjoocomver:2de0cb97c78c736c1a83986d9eed35930dcf2f7e1752ab246a1f07a2c1df80d4@ec2-54-235-67-106.compute-1.amazonaws.com:5432/d7371r9etk7t03',
+    client: 'sqlite3',
+    connection: {
+      filename: './db/database.sqlite3'
+    },
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: './db/seeds'
     },
     useNullAsDefault: true,
-    ssl: true
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
+ 
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: dbConnection,
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      tableName: 'knex_migrations',
+      directory: './db/migrations',
     }
   }
 
