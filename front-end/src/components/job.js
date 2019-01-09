dbb8c386b96ef67985d0dccdcc7000d7ccdadcf8
Brian G <brian.gregorius@gmail.com>
2019-01-09 11:00:42 -0500

    added a different layout to the front-end. Jobs page renders jobs with fake data and an individual job can be viewed

---
+++ front-end/src/components/job.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Jobs = props => {
+
+    return(
+        <div>
+            <h3>{props.job.titleSalary}</h3>
+        </div>
+    )
+}
+
+export default Jobs;
--- front-end/src/components/jobList.js
+++ front-end/src/components/jobList.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Jobs from './job.js';
 
-
-
-const jobList = props => {
+const JobList = props => {
     return (
         <div>
-
+            <button>Post a job</button>
+            {props.jobs.map(job => (
+                <Link to={`/jobs/${job.id}`} id={job.id} key={job.id}>
+                    <Jobs job={job} id={job.id}/>
+                </Link>
+            ))}
         </div>
     )
 }
