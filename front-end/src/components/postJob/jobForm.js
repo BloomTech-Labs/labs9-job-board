2e00d4325fdedc59bc26bc97aae3534ca3e8cc59
Yusuf Nafey <yusufnafey@yahoo.com>
2019-01-09 14:57:47 -0800

    did some form styling

--- front-end/src/components/postJob/jobForm.js
+++ front-end/src/components/postJob/jobForm.js
@@ -1,92 +1,103 @@
 import React from 'react';
-// import styled from 'styled-components';
-
-// const PostJobForm = styled.form`
-//     margin: 5px 0;
-//     display: flex;
-//     justify-content: space-between;
-//     align-items: center;
-//     font-size: 9px;
-// `
+import {
+    PostJobForm, InputTitles, Input, InputContainer, TextArea
+} from './jobFormStyling';
 
 const JobForm = props => {
     return (
         <div>
-            <form onSubmit={props.addJob}>
-                    <h3>Title</h3>
-                    <input 
+            <PostJobForm onSubmit={props.addJob}>
+                <InputContainer>
+                    <InputTitles
+                        for='title'
+                    >Title</InputTitles>
+                    <Input 
+                        id='title'
                         value={props.title}
                         name='title'
                         onChange={props.handleInput}
                         placeholder='Job Title'                        
                     />
-                    <h3>Salary</h3>
-                    <input 
+                </InputContainer>
+                <InputContainer>
+                    <InputTitles>Salary</InputTitles>
+                    <Input 
                         value={props.salary}
                         name='salary'
                         onChange={props.handleInput}
                         placeholder='Salary Range'
                     />
-                    <h3>Top Skills</h3>
-                    <input 
+                </InputContainer>
+                <InputContainer>
+                    <InputTitles>Top Skills</InputTitles>
+                    <Input 
                         value={props.topSkills}
                         name='topSkills'
                         onChange={props.handleInput}
                         placeholder='Top Skills'
-                    />
-                    <h3>Additional Skills</h3>
-                    <input 
+                    />                   
+                </InputContainer>
+                <InputContainer>
+                    <InputTitles>AdditionalSkills</InputTitles>
+                    <Input 
                         value={props.additionalSkills}
                         name='additionalSkills'
                         onChange={props.handleInput}
                         placeholder='Additional Skills'
                     />
-                    <h3>Familiar With</h3>
-                    <input 
+                </InputContainer>
+                <InputContainer>
+                    <InputTitles>Familiar With</InputTitles>
+                    <Input 
                         value={props.familiarWith}
                         name='familiarWith'
                         onChange={props.handleInput}
                         placeholder='Remaining Skills'
                     />
-                    <h3>Description</h3>
-                    <textarea 
+                </InputContainer>
+                <InputContainer>
+                    <InputTitles>Description</InputTitles>
+                    <TextArea
                         value={props.description}
                         name='description'
                         onChange={props.handleInput}
                         placeholder='Job Description'
                     />
-                    <h3>Requirements</h3>
-                    <textarea 
+                </InputContainer>
+                <InputContainer>
+                    <InputTitles>Requirements</InputTitles>
+                    <TextArea
                         value={props.requirements}
                         name='requirements'
                         onChange={props.handleInput}
                         placeholder='Job Requirements'
                     />
-                    <h5>Active</h5>
-                    <input 
-                        type='checkbox'
-                        name='active'
-                        value={props.active}
-                    />
-                    <h5>This requires a degree</h5>
-                    <input 
-                        type='checkbox'
-                        name='requiresDegree'
-                        value={props.requiresDegree}
-                    />
-                    <button
-                        type='reset'
-                        onClick={props.handleInput}
-                    >
-                        Cancel
-                    </button>
-                    <button
-                        type='submit'
-                        onClick={props.handleInput}
-                    >
-                        Save
-                    </button>
-                </form>
+                </InputContainer>
+                <h5>Active</h5>
+                <Input 
+                    type='checkbox'
+                    name='active'
+                    value={props.active}
+                />
+                <h5>This requires a degree</h5>
+                <Input 
+                    type='checkbox'
+                    name='requiresDegree'
+                    value={props.requiresDegree}
+                />
+                <button
+                    type='reset'
+                    onClick={props.handleInput}
+                >
+                    Cancel
+                </button>
+                <button
+                    type='submit'
+                    onClick={props.handleInput}
+                >
+                    Save
+                </button>
+            </PostJobForm>
         </div>
     );
 };
