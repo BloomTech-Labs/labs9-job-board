32c32f353d1068f724fc46212b7ae550b40d995b
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-01-10 16:22:05 -0500

    reorganizes react files

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
-import JobsContainer from "./components/jobContainer/jobsContainer.js";
+import Routes from "./components/Routes/Routes.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <JobsContainer />
+        <Routes />
+        {/* <Footer /> */}
       </div>
     );
   }
