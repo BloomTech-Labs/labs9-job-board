dabe6253258691b5a9e1721ab3f311f795ae0255
Brian G <brian.gregorius@gmail.com>
2019-01-11 12:04:13 -0500

    started jobs seed

---
+++ backend/db/seeds/002-jobs.js
@@ -0,0 +1,7 @@
+exports.seed = function(knex) {
+  return knex("table_name").insert([
+    { id: 1, colName: "rowValue1" },
+    { id: 2, colName: "rowValue2" },
+    { id: 3, colName: "rowValue3" }
+  ]);
+};
