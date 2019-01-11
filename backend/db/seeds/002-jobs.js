c943e34769200198bdb2d0a26bb4eddbba83a882
Brian G <brian.gregorius@gmail.com>
2019-01-11 12:10:46 -0500

    seed populated

--- backend/db/seeds/002-jobs.js
+++ backend/db/seeds/002-jobs.js
@@ -1,7 +1,124 @@
 exports.seed = function(knex) {
   return knex("table_name").insert([
-    { id: 1, colName: "rowValue1" },
-    { id: 2, colName: "rowValue2" },
-    { id: 3, colName: "rowValue3" }
+    {
+      title: "rowValue1",
+      salary: "100",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 1
+    },
+    {
+      title: "rowValue1",
+      salary: "50",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 9
+    },
+    {
+      title: "rowValue1",
+      salary: "60",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 1
+    },
+    {
+      title: "rowValue1",
+      salary: "60,000",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 2
+    },
+    {
+      title: "rowValue1",
+      salary: "60,000",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 3
+    },
+    {
+      title: "rowValue1",
+      salary: "50,000",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 4
+    },
+    {
+      title: "rowValue1",
+      salary: "40,000",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 5
+    },
+    {
+      title: "rowValue1",
+      salary: "30,000",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 6
+    },
+    {
+      title: "rowValue1",
+      salary: "40,000",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 7
+    },
+    {
+      title: "rowValue1",
+      salary: "65,000",
+      topSkills: "",
+      addSkills: "",
+      familier: "",
+      description: "",
+      requirements: "",
+      active: "",
+      collegeDegree: "",
+      users_id: 8
+    }
   ]);
 };
