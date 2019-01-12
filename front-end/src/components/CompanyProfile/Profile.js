62907a70cd697b59346113a4a50630bc8b454670
Brian G <brian.gregorius@gmail.com>
2019-01-12 16:34:00 -0500

    added company profile page

---
+++ front-end/src/components/CompanyProfile/Profile.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  componentDidMount() {}
+
+  render() {
+    return <div />;
+  }
+}
+
+export default Profile;
