92e4a7a7902d410fb0d026f63f0b3781913c4b24
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-14 18:39:32 -0500

    refactored to handle change and handle submit for errors

--- front-end/src/components/billing/checkoutForm.js
+++ front-end/src/components/billing/checkoutForm.js
@@ -1,48 +1,25 @@
 import React, { Component } from 'react';
-import {
-	CardElement,
-	injectStripe,
-	CardNumberElement,
-	CardExpiryElement,
-	CardCVCElement,
-	Elements,
-} from 'react-stripe-elements';
-
-const createOptions = () => {
-	return {
-		style: {
-			base: {
-				fontSize: '16px',
-				color: '#424770',
-				letterSpacing: '0.025em',
-				'::placerholder': {
-					color: '#aab7c4',
-				},
-			},
-			invalid: {
-				color: '#c23d4b',
-			},
-		},
-	};
-};
+import { CardElement, injectStripe } from 'react-stripe-elements';
 
 class CheckoutForm extends Component {
-	constructor(props) {
-		super(props);
-		this.state = { complete: false };
-		this.submit = this.submit.bind(this);
-	}
+	state = {
+		errorMessage: '',
+	};
 
-	async submit(ev) {
-		let { token } = await this.props.stripe.createToken({ name: 'Name' });
-		let response = await fetch('/charge', {
-			method: 'POST',
-			HEADERS: { 'Content-Type': 'text/plain' },
-			body: token.id,
-		});
+	handleChange = ({ error }) => {
+		if (error) {
+			this.setState({ errorMessage: error.message });
+		}
+	};
 
-		if (response.ok) this.setState({ complete: true });
-	}
+	handleSubmit = evt => {
+		evt.preventDefault();
+		if (this.props.stripe) {
+			this.props.stripe.createToken().then(this.props.handleResult);
+		} else {
+			console.log("Stripe.js hasn't loaded yet.");
+		}
+	};
 
 	render() {
 		if (this.state.complete) return <h1>Purhcase Complete!</h1>;
