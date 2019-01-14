22381f4ce9b68e6e54f1aa1f7977ca8adc0a5b70
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-14 18:25:55 -0500

    created handleChange in handling errors

--- front-end/src/components/billing/checkoutForm.js
+++ front-end/src/components/billing/checkoutForm.js
@@ -1,34 +1,52 @@
-import React, {Component} from 'react';
-import {CardElement, injectStripe} from 'react-stripe-elements';
+import React, { Component } from 'react';
+import {
+	CardElement,
+	injectStripe,
+	CardNumberElement,
+	CardExpiryElement,
+	CardCVCElement,
+	Elements,
+} from 'react-stripe-elements';
 
-class CheckoutForm extends Component{
-    constructor(props){
-        super(props);
-        this.state= {complete: false};
-        this.submit = this.submit.bind(this);
-    }
+const createOptions = () => {
+	return {
+		style: {
+			base: {
+				fontSize: '16px',
+				color: '#424770',
+				letterSpacing: '0.025em',
+				'::placerholder': {
+					color: '#aab7c4',
+				},
+			},
+			invalid: {
+				color: '#c23d4b',
+			},
+		},
+	};
+};
 
-    async submit(ev){
-        let {token} = await this.props.stripe.createToken({name: 'Name'});
-        let response = await fetch('/charge', {
-            method: 'POST',
-            HEADERS: {"Content-Type" : 'text/plain'},
-            body: token.id
-        });
+class CheckoutForm extends Component {
+	state = {
+		errorMessage: '',
+	};
 
-        if (response.ok) this.setState({complete:true});
-    }
+	handleChange = ({ error }) => {
+		if (error) {
+			this.setState({ errorMessage: error.message });
+		}
+	};
 
-    render() {
-        if (this.state.complete) return <h1>Purhcase Complete!</h1>; 
-        
-        return (
-            <div className='checkout'>
-            <CardElement />
-            <button onClick={this.submit}>Buy</button>
-            </div>
-        )
-    }
+	render() {
+		if (this.state.complete) return <h1>Purhcase Complete!</h1>;
+
+		return (
+			<div className="checkout">
+				<CardElement />
+				<button onClick={this.submit}>Buy</button>
+			</div>
+		);
+	}
 }
 
 export default injectStripe(CheckoutForm);
