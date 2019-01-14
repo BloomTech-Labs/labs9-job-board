f74f3c38ab2f7d27461c350586ecf5e1ca806640
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-14 13:18:02 -0500

    created a post route to test amount entered if it is valid or not

--- backend/routers/stripeRouter.js
+++ backend/routers/stripeRouter.js
@@ -2,4 +2,18 @@ const express = require('express')();
 const stripe = require('stripe')('sk_test_W2k36bSR8IXQLEqa9IHJoCfz');
 const router = express.Router();
 
-router.use(require(body - parser).text());
+router.post('/charge', async (req, res) => {
+	try {
+		let { status } = await stripe.charges.create({
+			amount: 2000,
+			currency: 'usd',
+			description: 'An example cahrge',
+			source: req.body,
+		});
+		res.jsson({ status });
+	} catch (err) {
+		res.status(500).end();
+	}
+});
+
+router.listen(9000, () => console.log('Listening on port 9000'));
