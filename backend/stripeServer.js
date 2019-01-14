7f606453f993e04320f0212404c5d22898b80e6b
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-14 14:06:10 -0500

    deleted stripeServer.js and removed proxy for localhost:9000

--- backend/stripeServer.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-const server = require('./api/stripeServer.js');
-
-//uses 9000 for Stripe Server
-
-const port = process.env.PORT || 9000;
-server.listen(port, () => console.log(`\nServer listening on port ${port}\n`));
