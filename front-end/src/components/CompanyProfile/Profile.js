97431f98027633795a597dbc9f61b14c0f380082
SavannahGreuel <smallidgesavannah@gmail.com>
2019-01-15 10:54:15 -0700

    basic form layout for profile page

--- front-end/src/components/CompanyProfile/Profile.js
+++ front-end/src/components/CompanyProfile/Profile.js
@@ -1,35 +1,71 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React from "react";
+
 
 const url = process.env.REACT_APP_DB_URL;
 
-class Profile extends Component {
-  constructor() {
-    super();
-    this.state = {
-      users: null
-    };
-  }
-
-  componentDidMount() {
-    axios
-      .get(`${url}/api/users`)
-      .then(res => {
-        this.setState({ users: res.data });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
-
-  render() {
-    console.log(this.state);
-    return (
+const ProfileForm = props => {
+  return (
+    <div>
+      <form>
+        <input
+         type="image"
+         id="image" 
+         alt="Company Profile Photo"
+         />
+        <div>
+          <label>Email</label>
+          <input
+            id="email"
+            placeholder="user@gmail.com"
+          />
+        </div>
+        <div>
+          <label>First Name</label>
+          <input
+            id="firstName"
+            placeholder="John"
+          />
+        </div>
+        <div>
+          <label>Last Name</label>
+          <input
+            id="lastName"
+            placeholder="Doe"
+          />
+        </div>
+        <div>
+          <label>Company Name</label>
+          <input
+            id="companyName"
+            placeholder="Lambda Technologies"
+          />
+        </div>
+        <div>
+          <label>Company Summary</label>
+          <textarea
+            id="companySummary"
+            placeholder="Briefley describe your company for your potential employees"
+          />
+        </div>
+        <div>
+          <label>Applications Inbox</label>
+          <input
+            id="applicationInbox"
+            placeholder="apply@company.com"
+          />
+        </div>
+        <button type="submit"> Save </button>
+        
+      </form>
       <div>
-        <h1>coming soon</h1>
+        <p>Old Password</p>
+        <p>New Password</p>
+        <p>Confirm Password</p>
       </div>
-    );
-  }
+      <button> Billing and Jobs </button>
+
+    </div>
+  )
 }
 
-export default Profile;
+export default ProfileForm;
