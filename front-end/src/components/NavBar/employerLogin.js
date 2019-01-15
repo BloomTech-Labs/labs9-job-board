e59a75df3ac4a8b8a76bbb4cccb2f8375a04061e
Yusuf Nafey <yusufnafey@yahoo.com>
2019-01-15 14:34:59 -0800

    started to make a hamburger nav

--- front-end/src/components/NavBar/employerLogin.js
+++ front-end/src/components/NavBar/employerLogin.js
@@ -1,14 +1,46 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./navBar.css";
+import Routes from "../Routes/Routes";
+
+// <div className="employer-login-container">
+//   <h3>Employers</h3>
+//   <div className="login-buttons-container">
+//     <Link to="/sign-in">Log In</Link>
+//     <Link to="/sign-up">Sign Up</Link>
+//   </div>
+// </div>
 
 const EmployerLogin = props => {
   return (
-    <div className="employer-login-container">
-      <h3>Employers</h3>
-      <div className="login-buttons-container">
-        <Link to="/sign-in">Log In</Link>
-        <Link to="/sign-up">Sign Up</Link>
+    <div class="hamburger">
+      <label for="toggle">&#9776;</label>
+      <input type="checkbox" id="toggle" />
+      <div class="menu">
+        <Link className="nav-links" to="/">
+          Home
+        </Link>
+        <Link className="nav-links" to="/jobs/:id">
+          Single Job
+        </Link>
+        <Link className="nav-links" to="/post-job">
+          Post Job
+        </Link>
+        <Link className="nav-links" to="/billing">
+          Billing
+        </Link>
+        <Link className="nav-links" to="/account">
+          Account
+        </Link>
+        <Link className="nav-links" to="/sign-up">
+          Sign Up
+        </Link>
+        <Link className="nav-links" to="/sign-in">
+          Sign In
+        </Link>
+        <Link className="nav-links" to="/reset-password">
+          Reset Password
+        </Link>
       </div>
     </div>
   );
