ada82eebe2a07b05e6ef7ce8e37abf2ca461ef67
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-15 11:50:19 -0500

    removed elements wrapper around checkout.js so stripe provider can have access to the stripe objects in checkout.js

--- front-end/src/components/billing/billing.js
+++ front-end/src/components/billing/billing.js
@@ -1,16 +1,12 @@
 import React, { Component } from 'react';
-import { Elements, StripeProvider } from 'react-stripe-elements';
+import { StripeProvider } from 'react-stripe-elements';
 import CheckoutForm from './checkoutForm';
 
 class Billing extends Component {
 	render() {
 		return (
 			<StripeProvider apiKey="pk_test_Q92ozglyNRHHwz44yCal2sV7">
-				<div>
-					<Elements>
-						<CheckoutForm />
-					</Elements>
-				</div>
+				<CheckoutForm />
 			</StripeProvider>
 		);
 	}
