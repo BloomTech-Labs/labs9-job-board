bd8e525b61ebeefa1bada0a0b00f84a08c9226d1
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-15 11:56:09 -0500

    created a new billingCheckout form and added that to billing.js replacing the checkoutForm.js

--- front-end/src/components/billing/billing.js
+++ front-end/src/components/billing/billing.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { StripeProvider } from 'react-stripe-elements';
-import CheckoutForm from './checkoutForm';
+import BillingCheckout from './billingCheckout';
 
 class Billing extends Component {
 	render() {
 		return (
 			<StripeProvider apiKey="pk_test_Q92ozglyNRHHwz44yCal2sV7">
-				<CheckoutForm />
+				<BillingCheckout />
 			</StripeProvider>
 		);
 	}
