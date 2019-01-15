e8e1ed39bd1cade56c7296fd1b5c6664a1cc0b87
Brian G <brian.gregorius@gmail.com>
2019-01-15 14:57:56 -0500

    added state and onDrop method

--- front-end/src/components/CompanyProfile/profilePic.js
+++ front-end/src/components/CompanyProfile/profilePic.js
@@ -1,17 +1,37 @@
 import React, { Component } from "react";
-import DropZone from "react-dropzone";
+import Dropzone from "react-dropzone";
 import request from "superagent";
 
+const cloudUrl = process.env.REACT_APP_CLOUD_PRESET;
+const cloudUpload = process.env.REACT_APP_CLOUD_UPLOAD;
+
 class ProfilePic extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      uploadedPic: null,
+      picUrl: ""
+    };
+  }
+
+  onDrop(files) {
+    this.setState({
+      uploadedPic: files[0]
+    });
+  }
   render() {
-    <form>
-      <Dropzone
-        multipe={false}
-        accept="image/*"
-        onDrop={this.onImageDrop.bind(this)}
-      >
-        <p>Upload a profile picture</p>
-      </Dropzone>
-    </form>;
+    return (
+      <form>
+        <Dropzone
+          multipe={false}
+          accept="image/jpg, image/png"
+          onDrop={this.onImageDrop.bind(this)}
+        >
+          <p>Upload a profile picture</p>
+        </Dropzone>
+      </form>
+    );
   }
 }
+
+export default ProfilePic;
