7e96746f395558df8e958bc9bf4870aa83b9d5fc
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-16 16:07:42 -0500

    imported axios and StripeCheckout into billingCheckout.js

--- front-end/src/components/billing/billingCheckout.js
+++ front-end/src/components/billing/billingCheckout.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+import StripeCheckout from 'react-stripe-checkout';
 import { Elements } from 'react-stripe-elements';
 
 import CheckoutForm from './checkoutForm';
