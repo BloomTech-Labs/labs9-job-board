cf65052a64da8cfc09f8fd470372f0a89d2fa367
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-16 16:59:57 -0500

    removed all imports

--- front-end/src/components/billing/billingCheckout.js
+++ front-end/src/components/billing/billingCheckout.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import axios from 'axios';
-import StripeCheckout from 'react-stripe-checkout';
+
 import { Elements } from 'react-stripe-elements';
 
 import CheckoutForm from './checkoutForm';
-import STRIPE_PUBLISHABLE from './stripe';
-import PAYMENT_SERVER_URL from './server';
 
 class BillingCheckout extends React.Component {
 	render() {
