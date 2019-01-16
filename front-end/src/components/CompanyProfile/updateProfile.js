c88214b2d22e57406645b5b0987d0bc8faf7de25
SavannahGreuel <smallidgesavannah@gmail.com>
2019-01-16 14:08:41 -0700

    made updates to updateProfile.js

--- front-end/src/components/CompanyProfile/updateProfile.js
+++ front-end/src/components/CompanyProfile/updateProfile.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import ProfileForm from "./profileForm";
-import ProfilePic from "./profilePic";
+// import ProfilePic from "./profilePic";
 
 const url = process.env.REACT_APP_DB_URL;
 
@@ -21,20 +21,7 @@ class UpdateProfile extends Component {
         };
     }
 
-    componentDidMount() {
-        const id = this.props.match.params.id
-        this.getUser(id);
-    }
-
-    getUser = id => {
-        axios 
-            .get(`${url}/api/users/${id}`)
-            .then(res => {
-                this.setState(() => ({user:res.data}))
-            })
-            .catch(error => console.log(error))
-    }
-
+   
     updateUser = e => {
         e.preventDefault();
         const id = this.props.match.params.id;
@@ -66,7 +53,7 @@ class UpdateProfile extends Component {
           return(
               <div>
                   <ProfileForm />
-                  <ProfilePic />
+                  {/* <ProfilePic /> */}
                   
               </div>
           )
--- front-end/src/constants/routes.js
+++ front-end/src/constants/routes.js
@@ -1,7 +1,7 @@
 export const LANDING = "/";
 export const JOB = "/jobs/:id";
 export const POST_JOB = "/post-job";
-export const COMPANY_PROFILE = "/profile";
+// export const COMPANY_PROFILE = "/profile";
 export const BILLING = "/billing";
 export const SIGN_UP = "/sign-up";
 export const SIGN_IN = "/sign-in";
