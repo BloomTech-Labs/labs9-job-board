ca4aa28539686da251214902dcf4ff86e8f79b85
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-16 17:47:19 -0500

    added StripeCheckout and its labels to be displayed for user

--- front-end/src/components/billing/billingCheckout.js
+++ front-end/src/components/billing/billingCheckout.js
@@ -21,9 +21,34 @@ const billingCheckout = () => {
 			})
 			.catch(error => {
 				console.log('Payment Error: ', error);
-				alert('payment Error');
+				alert('Payment Error');
 			});
 	};
+	return (
+		<StripeCheckout
+			label="Unlimited Jobs" //Component button text
+			name="KWC" //Modal Header
+			description="Unlimited Jobs (1 month)"
+			panelLabel="Unlimited Jobs" //Submit button in modal
+			amount={29999} //Amount in cents $299.99
+			token={onToken}
+			stripeKey={publishableKey}
+			label="Post Jobs" //Component button text
+			name="KWC" //Modal Header
+			description="Post Jobs (12)"
+			panelLabel="Post Jobs" //Submit button in modal
+			amount={9999} //Amount in cents $99.99
+			token={onToken}
+			stripeKey={publishableKey}
+			label="Post Job" //Component button text
+			name="KWC" //Modal Header
+			description="Post a Job"
+			panelLabel="Post Job" //Submit button in modal
+			amount={999} //Amount in cents $9.99
+			token={onToken}
+			stripeKey={publishableKey}
+		/>
+	);
 };
 
 export default billingCheckout;
