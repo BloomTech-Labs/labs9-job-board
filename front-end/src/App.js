fab81c764e33904514cafce404dbc26a823bd79a
Yusuf Nafey <yusufnafey@yahoo.com>
2019-01-16 11:22:10 -0800

    fixed issue

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,27 +2,23 @@ import React, { Component } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
-import { withAuthentication } from './components/Session';
-import { AuthenticatedUserContext } from './components/Session/index.js';
+import { withAuthentication } from "./components/Session";
+import { AuthenticatedUserContext } from "./components/Session/index.js";
 
 import Routes from "./components/Routes/Routes.js";
-import NavBar from "./components/NavBar/navBar";
-import SignOut from './components/SignOut/SignOut.js';
+import Toolbar from "./components/Toolbar/Toolbar";
+import SignOut from "./components/SignOut/SignOut.js";
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-          <NavBar />
+          <Toolbar />
           <AuthenticatedUserContext.Consumer>
-            {authenticatedUser => authenticatedUser ?
-              <SignOut />
-              :
-              null
-            }
+            {authenticatedUser => (authenticatedUser ? <SignOut /> : null)}
           </AuthenticatedUserContext.Consumer>
-          <Routes />
+          <Routes className="routes" />
           {/* <Footer /> */}
         </div>
       </Router>
