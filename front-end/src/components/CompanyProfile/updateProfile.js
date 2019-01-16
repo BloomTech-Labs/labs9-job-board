030eba2e5f51a5e13a2b355dddae903e86620019
SavannahGreuel <smallidgesavannah@gmail.com>
2019-01-16 11:36:42 -0700

    added newProfileForm page

--- front-end/src/components/CompanyProfile/updateProfile.js
+++ front-end/src/components/CompanyProfile/updateProfile.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
-import ProfileForm from "./Profile";
+import ProfileForm from "./profileForm";
 
 const url = process.env.REACT_APP_DB_URL;
 
