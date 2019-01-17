4a95861ae2162d1aa83882e8b41cd53e09cd67c9
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-01-17 14:26:21 -0500

    navigation menu options change based on log in status

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,11 +3,9 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import "./App.css";
 import { withAuthentication } from "./components/Session";
-import { AuthenticatedUserContext } from "./components/Session/index.js";
 
 import Routes from "./components/Routes/Routes.js";
 import Navigation from "./components/Navigation/Navigation";
-import SignOut from "./components/SignOut/SignOut.js";
 
 class App extends Component {
   render() {
@@ -15,17 +13,12 @@ class App extends Component {
       <Router>
         <div className="App">
           <Navigation />
-          <AuthenticatedUserContext.Consumer>
-            {authenticatedUser =>  (authenticatedUser ? <SignOut /> : null)}
-          </AuthenticatedUserContext.Consumer>
           <Routes className="routes" />
           {/* <Footer /> */}
         </div>
       </Router>
-    )
+    );
   }
 }
-
-
 
 export default withAuthentication(App);
--- front-end/src/components/Navigation/SideMenu.js
+++ front-end/src/components/Navigation/SideMenu.js
@@ -1,6 +1,10 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { AuthenticatedUserContext } from "../Session/index.js";
+import SignOut from "../SignOut/SignOut.js";
 import "./NavigationStyles.css";
+
+import * as ROUTES from "../../constants/routes";
 
 const sideMenu = props => {
   let sideMenuClass = "side-menu";
@@ -10,32 +14,48 @@ const sideMenu = props => {
   }
   return (
     <nav className={sideMenuClass}>
-      <div className="side-menu-navigation-items">
-        <NavLink onClick={props.click} to="/">
-          Home
-        </NavLink>
-        <NavLink onClick={props.click} to="/jobs/:id">
-          Single Job
-        </NavLink>
-        <NavLink onClick={props.click} to="/post-job">
-          Post Job
-        </NavLink>
-        <NavLink onClick={props.click} to="/billing">
-          Billing
-        </NavLink>
-        <NavLink onClick={props.click} to="/account">
-          Account
-        </NavLink>
-        <NavLink onClick={props.click} to="/sign-up">
-          Sign Up
-        </NavLink>
-        <NavLink onClick={props.click} to="/sign-in">
-          Sign In
-        </NavLink>
-        <NavLink onClick={props.click} to="/reset-password">
-          Reset Password
-        </NavLink>
-      </div>
+      <AuthenticatedUserContext.Consumer>
+        {authenticatedUser =>
+          authenticatedUser ? (
+            <div className="side-menu-navigation-items">
+              <NavLink onClick={props.click} to="/">
+                Home
+              </NavLink>
+              <NavLink onClick={props.click} to="/jobs/:id">
+                Single Job
+              </NavLink>
+              <NavLink onClick={props.click} to="/post-job">
+                Post Job
+              </NavLink>
+              <NavLink onClick={props.click} to="/billing">
+                Billing
+              </NavLink>
+              <NavLink onClick={props.click} to="/account">
+                Account
+              </NavLink>
+              <SignOut className="hamburger-button" />
+            </div>
+          ) : (
+            <div className="side-menu-navigation-items">
+              <NavLink onClick={props.click} to="/">
+                Home
+              </NavLink>
+              <NavLink onClick={props.click} to="/jobs/:id">
+                Single Job
+              </NavLink>
+              <NavLink onClick={props.click} to="/sign-up">
+                Sign Up
+              </NavLink>
+              <NavLink onClick={props.click} to="/sign-in">
+                Sign In
+              </NavLink>
+              <NavLink onClick={props.click} to="/reset-password">
+                Reset Password
+              </NavLink>
+            </div>
+          )
+        }
+      </AuthenticatedUserContext.Consumer>
     </nav>
   );
 };
