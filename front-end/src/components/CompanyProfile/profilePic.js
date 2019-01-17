2883afa9ebfefb32fa684eae5e25cfabfe39b04f
Brian G <brian.gregorius@gmail.com>
2019-01-17 07:59:27 -0500

    added method to upload photo

--- front-end/src/components/CompanyProfile/profilePic.js
+++ front-end/src/components/CompanyProfile/profilePic.js
@@ -8,6 +8,26 @@ class ProfilePic extends Component {
     };
   }
 
+  setUrl() {
+    console.log(this.state.profilePic[0].url);
+  }
+
+  uploadPhoto() {
+    let _this = this;
+    window.cloudinary.openUploadWidget(
+      { cloud_name: "dg9vhfqmb", upload_preset: "dzsxr7v2" },
+      function(error, result) {
+        console.log(result);
+        if (result) {
+          _this.setState({ profilePic: _this.state.profilePic.concat(result) });
+          _this.setUrl();
+        } else {
+          console.log(error);
+        }
+      }
+    );
+  }
+
   render() {
     return <div />;
   }
