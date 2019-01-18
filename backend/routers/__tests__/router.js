4fdbb4fe417f23610f8a0e7adbd31bcbdce4445d
Yusuf Nafey <yusufnafey@yahoo.com>
2019-01-18 08:03:01 -0800

    cleaned up

--- backend/routers/__tests__/router.js
+++ backend/routers/__tests__/router.js
@@ -4,29 +4,21 @@ const server = require("../../api/server.js");
 
 describe("router.js", () => {
   describe("get /job route", () => {
-    // it("returns status code 200", async () => {
-    //   let response = await request(server).get("/job");
-    //   expect(Array.isArray(response.body)).toBeTruthy();
-    // });
-    // it("returns json", async () => {
-    //   let response = await request(server).get("/job");
-    //   expect(response.type).toBe("application/json");
-    // });
-    it("returns status code 200 if successful", async () => {
+    it("returns status code 501", async () => {
       let response = await request(server).get("/job");
-      expect(response.status).toBe(200);
+      expect(response.status).toBe(501);
     });
   });
 
   describe("get /job/:id route", () => {
-    it("returns status code 200 if successful", async () => {
+    it("returns status code 500", async () => {
       let response = await request(server).get("/job/:id");
       expect(response.status).toBe(200);
     });
   });
 
   describe("post /job route", () => {
-    it("returns status code 500 if failed", async () => {
+    it("returns status code 500", async () => {
       let response = await request(server)
         .post("/")
         .send();
@@ -37,19 +29,6 @@ describe("router.js", () => {
   describe("delete /job:/id route", () => {
     it("returns status code 500", async () => {
       let response = await request(server).delete("/:id");
-      expect(response.status).toBe(500);
-    });
-    it("deletes user", async () => {
-      let response = await request(server)
-        .delete("/:id")
-        .send({ _id: 1 });
-      expect(response.body).toEqual({ message: `Job with ID 5 deleted.` });
-    });
-  });
-
-  describe("update /job/:id route", () => {
-    it("returns status code 500", async () => {
-      let response = await request(server).delete(":/id");
       expect(response.status).toBe(500);
     });
   });
