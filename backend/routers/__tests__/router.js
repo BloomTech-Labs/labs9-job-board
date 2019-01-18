583903de551b6a407d000aa61a136411f16cac76
Yusuf Nafey <yusufnafey@yahoo.com>
2019-01-18 01:27:21 -0800

    post route test

--- backend/routers/__tests__/router.js
+++ backend/routers/__tests__/router.js
@@ -17,4 +17,20 @@ describe("router.js", () => {
       expect(response.status).toBe(200);
     });
   });
+
+  describe("get /job/:id route", () => {
+    it("returns status code 200 if successful", async () => {
+      let response = await request(server).get("/job/:id");
+      expect(response.status).toBe(200);
+    });
+  });
+
+  describe("post /job route", () => {
+    it("returns status code 500 if failed", async () => {
+      let response = await request(server)
+        .post("/")
+        .send();
+      expect(response.status).toBe(500);
+    });
+  });
 });
