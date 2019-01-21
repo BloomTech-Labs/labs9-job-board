fd7eda3e1fa3177e8bfce12ce8f7a566d81bf6a4
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-20 20:51:16 -0500

    refactor - commented out server.js

--- backend/stripe/server.js
+++ backend/stripe/server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dotenv = require('dotenv');
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const dotenv = require('dotenv');
 
-dotenv.config();
+// dotenv.config();
 
-const app = express();
+// const app = express();
 
-app.use(cors());
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cors());
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
-app.use(bodyParser.json());
+// // parse application/json
+// app.use(bodyParser.json());
 
-const configureRoutes = require('./payment');
+// const configureRoutes = require('./payment');
 
-configureRoutes(app);
+// configureRoutes(app);
 
-app.listen(9000, error => {
-	if (error) throw error;
-	console.log('Server running on port ' + 9000);
-});
+// app.listen(9000, error => {
+// 	if (error) throw error;
+// 	console.log('Server running on port ' + 9000);
+// });
