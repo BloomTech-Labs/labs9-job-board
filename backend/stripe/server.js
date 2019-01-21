3ffb732d550cfbd30e5507fee99360ef2ef82ca6
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-20 21:20:05 -0500

    refactor - comment out body-parser

--- backend/stripe/server.js
+++ backend/stripe/server.js
@@ -26,7 +26,7 @@
 const app = require('express');
 const stripe = require('stripe')('sk_test_W2k36bSR8IXQLEqa9IHJoCfz');
 
-app.use(require('body-parser').text());
+// app.use(require('body-parser').text());
 
 app.get('/', async (req, res) => {
 	res.send({
