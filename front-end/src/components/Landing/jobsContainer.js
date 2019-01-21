29f57b0a0117cc2dfc92bcfe35fcdd39fce056c5
Brian G <brian.gregorius@gmail.com>
2019-01-21 13:04:43 -0500

    added tablet design of header

--- front-end/src/components/Landing/jobsContainer.js
+++ front-end/src/components/Landing/jobsContainer.js
@@ -1,54 +1,54 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
-import JobList from './jobList';
+import JobList from "./jobList";
 
-import Jobs from './job';
-import Search from './search';
+import Jobs from "./job";
+import Search from "./search";
 
-import Categories from './categories';
-import Header from './header';
+import Categories from "./categories";
+import Header from "./header";
 
 const url = process.env.REACT_APP_DB_URL;
 
 class JobsContainer extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			jobs: [],
-			searchJobs: [],
-			search: '',
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      jobs: [],
+      searchJobs: [],
+      search: ""
+    };
+  }
 
-	componentDidMount() {
-		axios
-			.get(`${url}/api/job`)
-			.then(res => {
-				console.log(res);
-				this.setState({ jobs: res.data });
-			})
-			.catch(err => {
-				console.log(err);
-			});
-	}
+  componentDidMount() {
+    axios
+      .get(`${url}/api/job`)
+      .then(res => {
+        console.log(res);
+        this.setState({ jobs: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
 
-	handleInput = event => {
-		this.setState({ [event.target.name]: event.target.value });
-	};
+  handleInput = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-	searchResults = event => {
-		this.handleInput(event);
-		this.setState(preState => {
-			const searchJobs = preState.jobs.filter(result => {
-				return result.title.includes(preState.search);
-			});
-			return { searchJobs: searchJobs };
-		});
-	};
+  searchResults = event => {
+    this.handleInput(event);
+    this.setState(preState => {
+      const searchJobs = preState.jobs.filter(result => {
+        return result.title.toLowerCase().includes(preState.search);
+      });
+      return { searchJobs: searchJobs };
+    });
+  };
 
   render() {
-    console.log("click", this.handleInput);
+    console.log("click", this.state.searchJobs);
     console.log("Search", this.state.search);
     return (
       <div className="jobs-container container">
