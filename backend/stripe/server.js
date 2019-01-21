17881b63df3cea3619ff89625e6668878a019909
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-20 20:54:36 -0500

    refactor - added app.post route

--- backend/stripe/server.js
+++ backend/stripe/server.js
@@ -27,3 +27,18 @@ const app = require('express')();
 const stripe = require('stripe')('sk_test_W2k36bSR8IXQLEqa9IHJoCfz');
 
 app.use(require('body-parser').text());
+
+app.post('/charge', async (req, res) => {
+	try {
+		let { status } = await stripe.charges.create({
+			amount: 29999,
+			currency: 'usd',
+			description: 'Unlimited Jobs - 100 tokens',
+			source: req.body,
+		});
+
+		res.json({ status });
+	} catch (err) {
+		res.status(500).end();
+	}
+});
