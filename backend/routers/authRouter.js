84b3549442bd8c7160624bcf55bbc036242560f5
Sarah Tennis <sarah.n.tennis@gmail.com>
2019-01-22 13:01:47 -0500

    [POST] /api/auth/hasAccountInfo to see if user exists in users table

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -3,9 +3,10 @@ const express = require("express");
 const db = require("../db/config");
 const router = express.Router();
 
-// [POST] /api/auth/login/:id
+// [POST] /api/auth/login
 // returns if provided user_uid and email exist in login table
-router.post("/login/:firebase_id", async (req, res, next) => {
+// updates email field or creates new user accordingly
+router.post("/login", async (req, res, next) => {
   const { user_uid, email } = req.body;
 
   try {
@@ -42,53 +43,29 @@ router.post("/login/:firebase_id", async (req, res, next) => {
     }
   } catch (error) {
     console.log(error);
+    res.status(500).json(error);
   }
 });
 
-// [POST] /api/auth/login
-router.post("/login", (req, res) => {
-  const newUser = { ...req.body };
-
-  if (newUser) {
-    db("login")
-      .insert(newUser)
-      .then(() => {
-        res.status(201).send(true);
-      })
-      .catch(err => {
-        if (err.code === 23505) {
-          res.status(400).send(false);
-        } else {
-          res.status(500).json({ message: "Error posting to table" });
-        }
-      });
-  }
-});
-
-// [GET] /api/auth/hasAccountInfo
-router.post("/hasAccountInfo", (req, res) => {
-  const user_uid = { ...req.body };
+// [POST] /api/auth/hasAccountInfo
+router.post("/hasAccountInfo", async (req, res) => {
+  const { user_uid } = req.body;
 
   if (user_uid) {
-    db("login")
-      .where(user_uid)
-      .then(res => {
-        if (!res.length) {
-          res.status(200).send(false);
-        } else {
-          return db("users").where(user_uid);
-        }
-      })
-      .then(res => {
-        if (!res.length) {
-          res.status(200).send(false);
-        } else {
-          res.status(200).send(true);
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ message: "Error determining first login" });
-      });
+    try {
+      const checkUser = await db("users").where({ user_uid });
+      checkUser.length
+        ? res
+            .status(200)
+            .json({ status: "exists in users", action: "redirect to landing" })
+        : res.status(200).json({
+            status: "no account information",
+            action: "redirect to new user"
+          });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error);
+    }
   }
 });
 
