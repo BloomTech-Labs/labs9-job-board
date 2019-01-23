263746b048ad340c5392c829d9024fede8916db9
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-23 17:45:59 -0500

    removed lined 27 and added back id to the body line 23

--- front-end/src/components/billing/checkoutForm.js
+++ front-end/src/components/billing/checkoutForm.js
@@ -20,12 +20,10 @@ class CheckoutForm extends Component {
 		let response = await fetch('http://localhost:9000/charge', {
 			method: 'POST',
 			headers: { 'Content-Type': 'text/plain' },
-			body: token,
+			body: token.id,
 		});
 		// if checkout is complete then message will be displayed
 		if (response.ok) this.setState({ complete: true });
-		if (response.ok === 'succeeded')
-			document.getElementById('shop').innerHTML = '<p>Purchase commplete!</p>';
 	}
 
 	render() {
