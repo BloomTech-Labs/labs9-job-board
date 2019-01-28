a0a8297d1eb66d04a1876fd897bcf166c93db644
Brian G <brian.gregorius@gmail.com>
2019-01-28 16:59:46 -0500

    refactored div's

--- front-end/src/components/CompanyProfile/profileInfo.js
+++ front-end/src/components/CompanyProfile/profileInfo.js
@@ -2,21 +2,39 @@ import React from "react";
 
 const ProfileInfo = props => {
   return (
-    <div className="companyinfo">
-      <div className="pic-job-balance">
-        <p>Job Postings: {props.company.balance}</p>
+    <div className="companyInfo">
+      <div className="pic-account">
         <img src={props.company.avatar_image} className="avatar" />
-        <button onClick={props.openEditor} className="editBtn">
-          Edit Profile
-        </button>
+        <h2>Your Account</h2>
       </div>
       <div className="all-company-info">
-        <p className="company-text">{props.company.first_name}</p>
-        <p className="company-text">{props.company.last_name}</p>
-        <p className="company-text">{props.company.email}</p>
-        <p className="company-text">{props.company.company_name}</p>
-        <p className="company-text">{props.company.summary}</p>
-        <p className="company-text">{props.company.application_method}</p>
+        <div className="company-text">
+          <label>First Name</label>
+          <p>{props.company.first_name}</p>
+          <label>Last Name</label>
+          <p>{props.company.last_name}</p>
+          <label>Email</label>
+          <p>{props.company.email}</p>
+          <label>Company name</label>
+          <p>{props.company.company_name}</p>
+          <label>Email for Incoming Applications</label>
+          <p>{props.company.application_method}</p>
+        </div>
+        <div className="summary-btns">
+          <label>Company Summary</label>
+          <p>{props.company.summary}</p>
+          <div className="company-btns">
+            <button onClick={props.openEditor} className="edit-btn edit">
+              Edit Account
+            </button>
+            <button onClick={props.openEditor} className="edit-btn bill">
+              Billing
+            </button>
+            <button onClick={props.openEditor} className="edit-btn pass">
+              Change Password
+            </button>
+          </div>
+        </div>
       </div>
     </div>
   );
