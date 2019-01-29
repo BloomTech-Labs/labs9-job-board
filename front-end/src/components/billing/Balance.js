6aa21ffe7c45727a7719c0c672556b673d3346da
Yusuf Nafey <yusufnafey@yahoo.com>
2019-01-29 13:40:55 -0800

    fixed nesting of divs

--- front-end/src/components/billing/Balance.js
+++ front-end/src/components/billing/Balance.js
@@ -1,73 +1,73 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
 const URL = process.env.REACT_APP_DB_URL;
 
 class Balance extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			balance: '',
-			fetching: false,
-			attempted: false,
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      balance: "",
+      fetching: false,
+      attempted: false
+    };
+  }
 
-	fetchBalance() {
-		this.setState({ fetching: true, attempted: true }, () => {
-			axios
-				.get(`${URL}/api/billing/balance/${this.props.authUser.uid}`)
-				.then(response => {
-					if (response.data.balance) {
-						this.setState({ balance: response.data, fetching: false });
-					} else {
-						this.setState({
-							balance: response.data,
-							fetching: false,
-						});
-					}
-				})
-				.catch(error => {
-					this.setState({
-						message: 'Error retrieving jobs',
-						fetching: false,
-					});
-				});
-		});
-	}
+  fetchBalance() {
+    this.setState({ fetching: true, attempted: true }, () => {
+      axios
+        .get(`${URL}/api/billing/balance/${this.props.authUser.uid}`)
+        .then(response => {
+          if (response.data.balance) {
+            this.setState({ balance: response.data, fetching: false });
+          } else {
+            this.setState({
+              balance: response.data,
+              fetching: false
+            });
+          }
+        })
+        .catch(error => {
+          this.setState({
+            message: "Error retrieving jobs",
+            fetching: false
+          });
+        });
+    });
+  }
 
-	formatDate(date) {
-		const newDate = new Date(date);
-		let month = newDate.getMonth() + 1;
-		let day = newDate.getDate();
-		let year = newDate.getFullYear();
+  formatDate(date) {
+    const newDate = new Date(date);
+    let month = newDate.getMonth() + 1;
+    let day = newDate.getDate();
+    let year = newDate.getFullYear();
 
-		return `${month}/${day}/${year}`;
-	}
+    return `${month}/${day}/${year}`;
+  }
 
-	componentDidMount() {
-		if (this.props.authUser) {
-			this.fetchBalance();
-		}
-	}
+  componentDidMount() {
+    if (this.props.authUser) {
+      this.fetchBalance();
+    }
+  }
 
-	componentDidUpdate() {
-		if (this.props.authUser && !this.state.attempted) {
-			this.fetchBalance();
-		}
-	}
+  componentDidUpdate() {
+    if (this.props.authUser && !this.state.attempted) {
+      this.fetchBalance();
+    }
+  }
 
-	render() {
-		return this.props.authUser ? (
-			<div className="right-side">
-				<div className="balance-container">
-					<span>Balance: 10 postings</span>
-				</div>
-			</div>
-		) : (
-			<div>Loading...</div>
-		);
-	}
+  render() {
+    return this.props.authUser ? (
+      <div className="billing-container">
+        <div className="balance-container">
+          <span>Balance: 10 postings</span>
+        </div>
+      </div>
+    ) : (
+      <div>Loading...</div>
+    );
+  }
 }
 
 export default Balance;
--- front-end/src/components/billing/billing.js
+++ front-end/src/components/billing/billing.js
@@ -1,34 +1,30 @@
-import React, { Component } from 'react';
-import { Elements, StripeProvider } from 'react-stripe-elements';
+import React, { Component } from "react";
+import { Elements, StripeProvider } from "react-stripe-elements";
 
-import CheckoutForm from './checkoutForm';
-import UserJobs from './UserJobs.js';
-import Balance from './Balance.js';
+import CheckoutForm from "./checkoutForm";
 
-import './checkoutForm.scss';
+import "./checkoutForm.scss";
 
 // const stripeurl = process.env.REACT_APP_STRIPE_TEST_KEY;
 
 class Billing extends Component {
-	constructor(props) {
-		super(props);
-	}
+  constructor(props) {
+    super(props);
+  }
 
-	render() {
-		return (
-			<div>
-				{/* <Balance authUser={this.props.authUser} /> */}
-				<StripeProvider apiKey="pk_test_77iYkIzmRpuMiC1SxkCkMIBp">
-					<div className="example">
-						<Elements>
-							<CheckoutForm authUser={this.props.authUser} />
-						</Elements>
-					</div>
-				</StripeProvider>
-				<UserJobs authUser={this.props.authUser} />
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <StripeProvider apiKey="pk_test_77iYkIzmRpuMiC1SxkCkMIBp">
+          <div className="container">
+            <Elements>
+              <CheckoutForm authUser={this.props.authUser} />
+            </Elements>
+          </div>
+        </StripeProvider>
+      </div>
+    );
+  }
 }
 
 export default Billing;
