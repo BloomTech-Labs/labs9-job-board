62a16062d1c8fe562234fa900b4d52b1cbc40571
Charletta Bullard <charlettabullard12@outlook.com>
2019-01-31 14:18:01 -0500

    Refactored - userjobs render functionality using the temp-file and made a corr to checkoutform

--- front-end/src/components/billing/temp-file.js
+++ front-end/src/components/billing/temp-file.js
@@ -1,40 +1,15 @@
-//userjobs.js
-{
-  this.state.fetching ? (
-    <img src={LoadingBar} alt="loading bar" />
-  ) : this.state.jobs.length ? (
-    this.state.jobs.map(job => {
-      return (
-        <div className="job-links">
-          {/* // key={job.id} */}
-          <h3>Job Listings:</h3>
-          <Link to={`/jobs/${job.id}`}>
-            <div className="billing-job">
-              {/* {job.title} */}
-              Junior Web Developer
-            </div>
-          </Link>
-          <Link to={`/edit-job/${job.id}`}>Edit</Link>
-        </div>
-      );
-    })
-  ) : (
-    <div>{this.state.message}</div>
-  );
-}
-
 //Balance.js
 
 return this.props.authUser ? (
-  <div className="balance-container">
-    <span>Balance:</span>
-    {this.state.balance.expiration
-      ? `Unlimited until ${this.formatDate(this.state.balance.expiration)}`
-      : ""}
-    <span className={this.state.balance.expiration ? "strikethrough" : ""}>
-      {`${this.state.balance.balance} postings`}
-    </span>
-  </div>
+	<div className="balance-container">
+		<span>Balance:</span>
+		{this.state.balance.expiration
+			? `Unlimited until ${this.formatDate(this.state.balance.expiration)}`
+			: ''}
+		<span className={this.state.balance.expiration ? 'strikethrough' : ''}>
+			{`${this.state.balance.balance} postings`}
+		</span>
+	</div>
 ) : (
-  <div>Loading...</div>
+	<div>Loading...</div>
 );
