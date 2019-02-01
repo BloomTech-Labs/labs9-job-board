75fd88cff78ef9088d2fd7f2d0f832fc1bb381b1
Charletta Bullard <charlettabullard12@outlook.com>
2019-02-01 13:05:45 -0500

    commented logic into PaymentModal.js

--- front-end/src/components/billing/PaymentModal.js
+++ front-end/src/components/billing/PaymentModal.js
@@ -1,43 +1,44 @@
-import React from "react";
+import React from 'react';
 
+//This is the payment modal logic that pops up once a payment has either been successful or if the payment has failed
 const PaymentModal = props => {
-  return (
-    <div
-      className={`payment-modal-container${
-        props.successVisible
-          ? " visible"
-          : props.failureVisible
-          ? " visible"
-          : ""
-      }`}
-    >
-      <div
-        className={`payment-modal-card${
-          props.successVisible
-            ? " visible"
-            : props.failureVisible
-            ? " visible"
-            : ""
-        }`}
-      >
-        <span
-          className={`payment-modal-header${
-            props.successVisible
-              ? " success"
-              : props.failureVisible
-              ? " failure"
-              : ""
-          }`}
-        >
-          {props.successVisible
-            ? "PAYMENT SUCCESSFULLY PROCESSED"
-            : props.failureVisible
-            ? "FAILED TO PROCESS PAYMENT"
-            : null}
-        </span>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className={`payment-modal-container${
+				props.successVisible
+					? ' visible'
+					: props.failureVisible
+					? ' visible'
+					: ''
+			}`}
+		>
+			<div
+				className={`payment-modal-card${
+					props.successVisible
+						? ' visible'
+						: props.failureVisible
+						? ' visible'
+						: ''
+				}`}
+			>
+				<span
+					className={`payment-modal-header${
+						props.successVisible
+							? ' success'
+							: props.failureVisible
+							? ' failure'
+							: ''
+					}`}
+				>
+					{props.successVisible
+						? 'PAYMENT SUCCESSFULLY PROCESSED'
+						: props.failureVisible
+						? 'FAILED TO PROCESS PAYMENT'
+						: null}
+				</span>
+			</div>
+		</div>
+	);
 };
 
 export default PaymentModal;
