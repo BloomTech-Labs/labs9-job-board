4ddc7a1dab8251667d72f2c77264ae1bb480ea34
Yusuf Nafey <yusufnafey@yahoo.com>
2019-02-04 09:52:07 -0800

    navigation comments

--- front-end/src/components/Navigation/SideMenuToggleButton.js
+++ front-end/src/components/Navigation/SideMenuToggleButton.js
@@ -2,6 +2,7 @@ import React from "react";
 
 const SideMenuToggleButton = props => {
   return (
+    // Allows side menu to be toggled between opened and closed
     <button className="toggle-button" onClick={props.click}>
       <div className="toggle-button-line" />
       <div className="toggle-button-line" />
